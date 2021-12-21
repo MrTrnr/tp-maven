2fb03a909c212624ff2b6e2d322a842390e8e782
MrTrnr <marietournier.pro@gmail.com>
2021-12-21 09:22:42 +0100

    TP 3 terminé

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -8,8 +10,12 @@ import com.github.lalyos.jfiglet.FigletFont;
  */
 public class App {
 	public static void main(String[] args) {
-		String asciiArt = FigletFont.convertOneLine("hello");
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
 		System.out.println(asciiArt);
-		System.out.println("Hello World!");
+
+		// affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
 	}
 }
